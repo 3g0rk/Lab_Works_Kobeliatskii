bc3b46f5e4dbff6f2274a1a7ba1e34dfc535ec4c
Egor Kobeliatskii <94761567+3g0rk@users.noreply.github.com>
2024-11-12 00:41:00 +0300

    Лабораторная работа 4

---
+++ Lab4_Task2.py
@@ -0,0 +1,14 @@
+# TODO решите задачу
+import json
+
+
+def task() -> float:
+    sum_of_items = 0
+    with open("input.json", "r") as f:
+        data = json.load(f)
+        for item in data:
+            sum_of_items += item["score"] * item["weight"]
+    return round(sum_of_items, 3)
+
+
+print(task())
